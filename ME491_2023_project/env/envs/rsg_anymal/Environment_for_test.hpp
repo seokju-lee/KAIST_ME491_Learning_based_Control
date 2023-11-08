5943fd25491f541fa13d51212eac94a313099191
seokju lee <dltjrwn0322@kaist.ac.kr>
2023-11-08 19:24:09 +0900

    Update Environment_for_test.hpp

--- ME491_2023_project/env/envs/rsg_anymal/Environment_for_test.hpp
+++ ME491_2023_project/env/envs/rsg_anymal/Environment_for_test.hpp
@@ -7,215 +7,169 @@
 
 #pragma once
 
-// raisim include
-#include "raisim/World.hpp"
-#include "raisim/RaisimServer.hpp"
-
-#include "../../Yaml.hpp"
+#include <set>
 #include "../../BasicEigenTypes.hpp"
-#include "../../Reward.hpp"
-#include PLAYER1_HEADER_FILE_TO_INCLUDE
-#include PLAYER2_HEADER_FILE_TO_INCLUDE
+#include "raisim/World.hpp"
 
 namespace raisim {
 
-class ENVIRONMENT_for_test {
+class AnymalController_for_test {
 
  public:
+  inline bool create(raisim::World *world) {
+    anymal_ = reinterpret_cast<raisim::ArticulatedSystem *>(world->getObject(name_));
 
-  explicit ENVIRONMENT_for_test(const std::string &resourceDir, const Yaml::Node &cfg, bool visualizable) :
-      visualizable_(visualizable) {
-    /// add objects
-    auto* robot = world_.addArticulatedSystem(resourceDir + "/anymal/urdf/anymal_red.urdf");
-    robot->setName(PLAYER1_NAME);
-    std::cout<<"player 1 name: "<<PLAYER1_NAME<<std::endl;
-    robot->setControlMode(raisim::ControlMode::PD_PLUS_FEEDFORWARD_TORQUE);
+    /// get robot data
+    gcDim_ = anymal_->getGeneralizedCoordinateDim();
+    gvDim_ = anymal_->getDOF();
+    nJoints_ = gvDim_ - 6;
 
+    /// initialize containers
+    gc_.setZero(gcDim_);
+    gc_init_.setZero(gcDim_);
+    gv_.setZero(gvDim_);
+    gv_init_.setZero(gvDim_);
+    pTarget_.setZero(gcDim_);
+    vTarget_.setZero(gvDim_);
+    pTarget12_.setZero(nJoints_);
+
+    /// this is nominal configuration of anymal
+    gc_init_ << 0, 0, 0.50, 1.0, 0.0, 0.0, 0.0, 0.03, 0.4, -0.8, -0.03, 0.4, -0.8, 0.03, -0.4, 0.8, -0.03, -0.4, 0.8;
+
+    /// set pd gains
+    Eigen::VectorXd jointPgain(gvDim_), jointDgain(gvDim_);
+    jointPgain.setZero();
+    jointPgain.tail(nJoints_).setConstant(50.0);
+    jointDgain.setZero();
+    jointDgain.tail(nJoints_).setConstant(0.2);
+    anymal_->setPdGains(jointPgain, jointDgain);
+    anymal_->setGeneralizedForce(Eigen::VectorXd::Zero(gvDim_));
+
+    /// MUST BE DONE FOR ALL ENVIRONMENTS
+    obDim_ = 48;
+    actionDim_ = nJoints_;
+    actionMean_.setZero(actionDim_);
+    actionStd_.setZero(actionDim_);
+    obDouble_.setZero(obDim_);
+
+    /// action scaling
+    actionMean_ = gc_init_.tail(nJoints_);
+    actionStd_.setConstant(0.1);
+
+    /// indices of links that should not make contact with ground
+    footIndices_.insert(anymal_->getBodyIdx("LF_SHANK"));
+    footIndices_.insert(anymal_->getBodyIdx("RF_SHANK"));
+    footIndices_.insert(anymal_->getBodyIdx("LH_SHANK"));
+    footIndices_.insert(anymal_->getBodyIdx("RH_SHANK"));
+
+    return true;
+  }
+
+  inline bool init(raisim::World *world) {
+    return true;
+  }
+
+  inline bool advance(raisim::World *world, const Eigen::Ref<EigenVec> &action) {
+    /// action scaling
+    pTarget12_ = action.cast<double>();
+    pTarget12_ = pTarget12_.cwiseProduct(actionStd_);
+    pTarget12_.setZero();
+    pTarget12_ += actionMean_;
+    pTarget_.tail(nJoints_) = pTarget12_;
+    anymal_->setPdTarget(pTarget_, vTarget_);
+    return true;
+  }
+
+  inline bool reset(raisim::World *world, const double &theta) {
+    if (playerNum_ == 0) {
+      gc_init_.head(3) << 1.5 * std::cos(theta), 1.5 * std::sin(theta), 0.5;
+      gc_init_.segment(3, 4) << cos((theta - M_PI) / 2), 0, 0, sin((theta - M_PI) / 2);
+    }
+    else {
+      gc_init_.head(3) << 1.5 * std::cos(theta + M_PI), 1.5 * std::sin(theta + M_PI), 0.5;
+      gc_init_.segment(3, 4) << cos(theta / 2), 0, 0, sin(theta / 2);
+    }
+    anymal_->setState(gc_init_, gv_init_);
+    return true;
+  }
+
+  inline void updateObservation(raisim::World *world) {
+    anymal_->getState(gc_, gv_);
+    raisim::Vec<4> quat, oppoquat;
+    raisim::Mat<3, 3> rot, opporot;
+    quat[0] = gc_[3];
+    quat[1] = gc_[4];
+    quat[2] = gc_[5];
+    quat[3] = gc_[6];
+    raisim::quatToRotMat(quat, rot);
+    bodyLinearVel_ = rot.e().transpose() * gv_.segment(0, 3);
+    bodyAngularVel_ = rot.e().transpose() * gv_.segment(3, 3);
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
-    /// if you want make opponent robot, use like below code (but in Environment.hpp, there exist only PLAYER_NAME in definition. So use any name for opponent robot name setting.
-    auto* dummy_robot = world_.addArticulatedSystem(resourceDir + "/anymal/urdf/anymal_blue.urdf");
-    dummy_robot->setName(PLAYER2_NAME);
-    std::cout<<"player 2 name: "<<PLAYER2_NAME<<std::endl;
-    dummy_robot->setControlMode(raisim::ControlMode::PD_PLUS_FEEDFORWARD_TORQUE);
+    /// if you want use opponent robot`s state, use like below code
+    auto opponent = reinterpret_cast<raisim::ArticulatedSystem *>(world->getObject(opponentName_));
+    opponent->getState(opponentGc, opponentGv);
+    oppoquat[0] = opponentGc[3];
+    oppoquat[1] = opponentGc[4];
+    oppoquat[2] = opponentGc[5];
+    oppoquat[3] = opponentGc[6];
+    raisim::quatToRotMat(oppoquat, opporot);
+    oppobodyLinearVel_ = opporot.e().transpose()*opponentGv.segment(0, 3);
+    oppobodyAngularVel_ = opporot.e().transpose()*opponentGv.segment(3, 3);
 
-
-    controller_.setName(PLAYER1_NAME);
-    controller_.setOpponentName(PLAYER2_NAME);
-    dummyController_.setName(PLAYER2_NAME);
-    dummyController_.setOpponentName(PLAYER1_NAME);
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-
-    auto* ground = world_.addGround();
-    ground->setName("ground");
-
-    controller_.create(&world_);
-    dummyController_.create(&world_);
-    READ_YAML(double, simulation_dt_, cfg["simulation_dt"])
-    READ_YAML(double, control_dt_, cfg["control_dt"])
-
-    /// visualize if it is the first environment
-    if (visualizable_) {
-      server_ = std::make_unique<raisim::RaisimServer>(&world_);
-      server_->launchServer();
-      auto cage = server_->addVisualCylinder("cage", 3.0, 0.1);
-      cage->setPosition(0,0,0);
-    }
+    obDouble_ << gc_.head(3), /// body pose
+        rot.e().row(2).transpose(), /// body orientation
+        gc_.tail(12), /// joint angles
+        bodyLinearVel_, bodyAngularVel_, /// body linear&angular velocity
+        gv_.tail(12), /// joint velocity
+        opponentGc.head(3), /// opponent pose
+        opporot.e().row(2).transpose(), /// opponent body orientation
+        oppobodyLinearVel_, oppobodyAngularVel_; /// oppponent body linear&angular velocity
   }
 
-  ~ENVIRONMENT_for_test() {
-    if(server_) server_->killServer();
+  inline const Eigen::VectorXd &getObservation() {
+    return obDouble_;
   }
 
-  void init() {}
-
-  void reset() {
-    double theta = uniDist_(gen_) * 2 * M_PI;
-//    theta = 0;
-    controller_.reset(&world_, theta);
-    dummyController_.reset(&world_, theta);
-    timer_ = 0;
+  void setName(const std::string &name) {
+    name_ = name;
   }
 
-  void step(const Eigen::Ref<EigenVec> &action) {
-    timer_ += 1;
-    controller_.advance(&world_, action);
-    dummyController_.advance(&world_, action);
-    for (int i = 0; i < int(control_dt_ / simulation_dt_ + 1e-10); i++) {
-      if (server_) server_->lockVisualizationServerMutex();
-      world_.integrate();
-      if (server_) server_->unlockVisualizationServerMutex();
-    }
-    controller_.updateObservation(&world_);
-    dummyController_.updateObservation(&world_);
+  void setOpponentName(const std::string &name) {
+    opponentName_ = name;
   }
 
-  void observe(Eigen::Ref<EigenVec> ob) {
-    controller_.updateObservation(&world_);
-    dummyController_.updateObservation(&world_);
-    ob = controller_.getObservation().cast<float>();
+  void setPlayerNum(const int &playerNum) {
+    playerNum_ = playerNum;
   }
 
-  bool player1_die() {
-    auto anymal = reinterpret_cast<raisim::ArticulatedSystem *>(world_.getObject(PLAYER1_NAME));
-    /// base contact with ground
-    for(auto& contact: anymal->getContacts()) {
-      if(contact.getPairObjectIndex() == world_.getObject("ground")->getIndexInWorld() &&
-          contact.getlocalBodyIndex() == anymal->getBodyIdx("base")) {
+  inline bool isTerminalState(raisim::World *world) {
+    for (auto &contact: anymal_->getContacts()) {
+      if (footIndices_.find(contact.getlocalBodyIndex()) == footIndices_.end()) {
         return true;
       }
     }
-    /// get out of the cage
-    int gcDim = anymal->getGeneralizedCoordinateDim();
-    Eigen::VectorXd gc;
-    gc.setZero(gcDim);
-    gc = anymal->getGeneralizedCoordinate().e();
-    if (gc.head(2).norm() > 3) {
-      return true;
-    }
     return false;
   }
 
-  bool player2_die() {
-    auto anymal = reinterpret_cast<raisim::ArticulatedSystem *>(world_.getObject(PLAYER2_NAME));
-    /// base contact with ground
-    for(auto& contact: anymal->getContacts()) {
-      if(contact.getPairObjectIndex() == world_.getObject("ground")->getIndexInWorld() &&
-          contact.getlocalBodyIndex() == anymal->getBodyIdx("base")) {
-        return true;
-      }
-    }
-    /// get out of the cage
-    int gcDim = anymal->getGeneralizedCoordinateDim();
-    Eigen::VectorXd gc;
-    gc.setZero(gcDim);
-    gc = anymal->getGeneralizedCoordinate().e();
-    if (gc.head(2).norm() > 3) {
-      return true;
-    }
-    return false;
+  inline int getObDim() {
+    return obDim_;
   }
 
-  bool isTerminalState() {
-    if (player1_die() && player2_die()) {
-      draw += 1;
-      terminal += 1;
-      std::cout<<"draw"<<std::endl;
-      return true;
-    }
-
-    if (timer_ > 10 * 100) {
-      draw += 1;
-      terminal += 1;
-      std::cout<<"draw"<<std::endl;
-      return true;
-    }
-
-
-    if (!player1_die() && player2_die()) {
-      player1_win += 1;
-      terminal += 1;
-      std::cout<<"player1 win"<<std::endl;
-      return true;
-    }
-
-    if (player1_die() && !player2_die()) {
-      player2_win += 1;
-      terminal += 1;
-      std::cout<<"player2 win"<<std::endl;
-      return true;
-    }
-
-    if (terminal == 10) {
-      std::cout<<"finish!"<<"\t"<<"player1 win: "<<player1_win<<"\t"<<"player2 win: "<<player2_win<<"\t"<<"draw: "<<draw<<std::endl;
-      std::cout<<"player1 get: "<<3 * player1_win + draw<<"\t"<<"player2 get: "<<3 * player2_win + draw<<std::endl;
-      exit(0);
-    }
-    return false;
+  inline int getActionDim() {
+    return actionDim_;
   }
-
-  void curriculumUpdate() {};
-
-  void close() { if (server_) server_->killServer(); };
-
-  void setSeed(int seed) {};
-
-  void setSimulationTimeStep(double dt) {
-    simulation_dt_ = dt;
-    world_.setTimeStep(dt);
-  }
-  void setControlTimeStep(double dt) { control_dt_ = dt; }
-
-  int getObDim() { return controller_.getObDim(); }
-
-  int getActionDim() { return controller_.getActionDim(); }
-
-  double getControlTimeStep() { return control_dt_; }
-
-  double getSimulationTimeStep() { return simulation_dt_; }
-
-  raisim::World *getWorld() { return &world_; }
-
-  void turnOffVisualization() { server_->hibernate(); }
-
-  void turnOnVisualization() { server_->wakeup(); }
-
-  void startRecordingVideo(const std::string &videoName) { server_->startRecordingVideo(videoName); }
-
-  void stopRecordingVideo() { server_->stopRecordingVideo(); }
 
  private:
-  int timer_ = 0;
-  int player1_win = 0, player2_win = 0, draw = 0, terminal = 0;
-  bool visualizable_ = false;
-  PLAYER1_CONTROLLER controller_;
-  PLAYER2_CONTROLLER dummyController_;
-  raisim::World world_;
-  double simulation_dt_ = 0.001;
-  double control_dt_ = 0.01;
-  std::unique_ptr<raisim::RaisimServer> server_;
-  std::uniform_real_distribution<double> uniDist_;
-  std::mt19937 gen_;
+  std::string name_, opponentName_;
+  int gcDim_, gvDim_, nJoints_, playerNum_ = 1;
+  raisim::ArticulatedSystem *anymal_;
+  Eigen::VectorXd gc_init_, gv_init_, gc_, gv_, opponentGc, opponentGv, pTarget_, pTarget12_, vTarget_;
+  Eigen::VectorXd actionMean_, actionStd_, obDouble_;
+  Eigen::Vector3d bodyLinearVel_, bodyAngularVel_;
+  Eigen::Vector3d oppobodyLinearVel_, oppobodyAngularVel_;
+  std::set<size_t> footIndices_;
+  int obDim_ = 0, actionDim_ = 0;
 };
-}
 
+}
